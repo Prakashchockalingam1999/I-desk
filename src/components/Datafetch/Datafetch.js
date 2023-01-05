8a3c29d8e8074ba80e6cae9658929ac6af4b94c6
Prakashchockalingam1999 <chockalingamprakash7@gmail.com>
2023-01-05 15:30:29 +0530

    second

--- src/components/Datafetch/Datafetch.js
+++ src/components/Datafetch/Datafetch.js
@@ -11,14 +11,13 @@ function Datafetch(){
         })
         .catch(err=>{
             console.log(err)
-        })
+        },[])
     })
     return(
         <div className="container-fluid bg-light">
             <div className="container">
                 <div className="row d-flex justify-content-center">
                  {post.map(post=>(<img style={{width:"300px", margin:"15px" }} key={post.id} src={post.thumbnailUrl}/>))}
-                 
                 </div>
             </div>
         </div>
